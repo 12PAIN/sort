9f4aaf9ecd61c56eba3f275037e324f13168977f
12PAIN <hexagon643@gmail.com>
2019-05-22 21:59:56 +0700

    End version of sort

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 using namespace std;
+
 static int v = 0;
+//static var for use in function
+
 void *sw(int arr[]){
     int temp;
     for (int i = 0; i < v - 1; i++) {
@@ -11,6 +14,7 @@ void *sw(int arr[]){
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+
             }
         }
     }
@@ -21,18 +25,20 @@ void *sw(int arr[]){
 int main()
 {
 
-
-
-    cout << "Enter the amout of massive: " ;
+    cout << "Enter the amount of massive elements: " ;
     cin >> v;
+
     if(v < 2){
-        cout << "Amount of massive can't be < 2" << endl;
+        cout << "Amount of massive elements can't be < 2" << endl;
         return 0;
     }
 
     int *array = new int[v-1];
 
+    cout << "Enter the values :";
+
     for(int i = 0;i<v;++i){
+        cout << "array[ "<< i << " ] = ";
         cin>>array[i];
     }
 
